854992bfda746e063eb5bb4e27ad2b7ee81102d2
achunbai <47388272+achunbai@users.noreply.github.com>
2023-12-02 21:34:08 +0800

    Update 2043：【例5.11】杨辉三角形.c
    修正错误

--- 信息学奥赛一本通/第五章 数 组/2043：【例5.11】杨辉三角形.c
+++ 信息学奥赛一本通/第五章 数 组/2043：【例5.11】杨辉三角形.c
@@ -75,7 +75,7 @@ int main()
 	for (int i = 0; i < n; i++)
 	{
 		a[i][0] = a[i][i] = 1;
-		for (int j = 1; j < i; i++)
+		for (int j = 1; j < i; j++)
 		{
 			a[i][j] = a[i - i][j] + a[i - i][j - 1];
 		}
@@ -83,7 +83,7 @@ int main()
 
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < i; j++)
+		for (int j = 0; j <= i; j++)
 		{
 			printf("%d ", a[i][j]);
 		}

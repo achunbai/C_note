2e8ce7bea51f4737e84fb475d9910a9868cdbcc7
achunbai <47388272+achunbai@users.noreply.github.com>
2024-01-01 01:00:43 +0800

    Update 1164：digit函数.c

--- 信息学奥赛一本通/第六章 函 数/1164：digit函数.c
+++ 信息学奥赛一本通/第六章 函 数/1164：digit函数.c
@@ -22,3 +22,34 @@
 【输出样例】
 8
 */
+#include <stdio.h>
+
+// digit函数的实现
+int digit(int n, int k)
+{
+    // 如果k不等于1，那么我们将n除以10，然后递归调用digit函数，k减1
+    if(k != 1)
+    {
+        return digit(n/10, k - 1);
+    }
+    // 如果k等于1，那么我们返回n除以10的余数，也就是n的最后一位数字
+    else if(k == 1)
+    {
+        return n % 10;
+    }
+    // 其他情况返回-1，实际上这种情况不会发生，因为题目已经限定了k的取值范围
+    else
+    {
+        return -1;
+    }
+}
+
+int main()
+{
+    int m, n;
+    // 读取输入的m和n
+    scanf("%d %d", &m, &n);
+    // 调用digit函数，打印结果
+    printf("%d\n", digit(m, n));
+    return 0;
+}

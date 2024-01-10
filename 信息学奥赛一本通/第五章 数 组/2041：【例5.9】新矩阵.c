a93e090ec522ff1919d4ddbb90eec3c74547582e
achunbai <47388272+achunbai@users.noreply.github.com>
2024-01-10 23:33:46 +0800

    Update 2041：【例5.9】新矩阵.c

--- 信息学奥赛一本通/第五章 数 组/2041：【例5.9】新矩阵.c
+++ 信息学奥赛一本通/第五章 数 组/2041：【例5.9】新矩阵.c
@@ -62,3 +62,35 @@ int main()
 	}
 	return 0;
 }
+
+/*
+// 课上C++代码
+#include<iostream>
+using namespace std;
+const int N = 1e4 + 10;
+int a[N][N];
+int main() {
+	int n; cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> a[i][j];
+		}
+	}
+	//模拟左上角到右下角的对角线
+	int x1 = 1, y1 = 1;
+	while (x1<=n&&y1<=n) a[x1++][y1++] += 10;
+	//模拟右上角到左下角的对角线
+	int x2 = 1, y2 = n;
+	while (x2 <= n && y2 >= 1) a[x2++][y2--] += 10;
+
+	//如果n是奇数，那么两条对角线一定会交于一点，按照题目要求，交点只处理一次
+	if (n % 2 == 1) a[n/2+1][n/2+1] -= 10;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cout << a[i][j] << " ";
+		}cout << endl;
+	}
+	return 0;
+}
+*/ 

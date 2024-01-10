4fc0284c2012ac2d42b9994a6d505ef1e164612a
achunbai <47388272+achunbai@users.noreply.github.com>
2024-01-10 23:33:51 +0800

    Update 2043：【例5.11】杨辉三角形.c

--- 信息学奥赛一本通/第五章 数 组/2043：【例5.11】杨辉三角形.c
+++ 信息学奥赛一本通/第五章 数 组/2043：【例5.11】杨辉三角形.c
@@ -70,24 +70,55 @@ int main()
 
 int main()
 {
-	int a[20][20], n;
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
-	{
-		a[i][0] = a[i][i] = 1;
-		for (int j = 1; j < i; j++)
-		{
-			a[i][j] = a[i - i][j] + a[i - i][j - 1];
+    int a[20][20], n; // 定义一个二维数组a和一个整数n
+
+    scanf("%d", &n); // 从标准输入读取一个整数n
+
+    // 使用两层循环来计算杨辉三角形的每一项
+    for (int i = 0; i < n; i++)
+    {
+        a[i][0] = a[i][i] = 1; // 杨辉三角形的每一行的第一项和最后一项都是1
+
+        // 计算杨辉三角形的其他项
+        for (int j = 1; j < i; j++)
+        {
+            a[i][j] = a[i - 1][j] + a[i - 1][j - 1]; // 杨辉三角形的每一项等于它上面的项和左上角的项的和
+        }
+    }
+
+    // 使用两层循环来打印杨辉三角形
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            printf("%d ", a[i][j]); // 打印杨辉三角形的每一项
+        }
+        printf("\n"); // 每打印完一行，就打印一个换行符
+    }
+    return 0;
+}
+
+/*
+// 课上C++代码
+#include<iostream>
+using namespace std;
+const int N = 1e2 + 10;
+int a[N][N];
+int main() {
+	int n; cin >> n;
+	a[1][1] = a[2][1] = a[2][2] = 1;
+	for (int i = 3; i <= n; i++) {
+		for (int j = 1; j <= i; j++) {
+			if (j == 1 || j == i) a[i][j] = 1;
+			else a[i][j] = a[i - 1][j] + a[i - 1][j - 1];
 		}
 	}
 
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j <= i; j++)
-		{
-			printf("%d ", a[i][j]);
-		}
-		printf("\n");
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= i; j++) {
+			cout << a[i][j] << " ";
+		}cout << endl;
 	}
 	return 0;
 }
+*/

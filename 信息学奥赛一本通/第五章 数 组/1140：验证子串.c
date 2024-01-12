683d096f450769e29c40216fb2a3ad1d0416bd1a
achunbai <47388272+achunbai@users.noreply.github.com>
2024-01-12 23:05:44 +0800

    Update 1140：验证子串.c

--- 信息学奥赛一本通/第五章 数 组/1140：验证子串.c
+++ 信息学奥赛一本通/第五章 数 组/1140：验证子串.c
@@ -45,3 +45,28 @@ int main()
     }
     return 0;
 }
+
+/*
+// 课上C++代码
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main() {
+
+	string s1, s2;
+	getline(cin, s1);
+	getline(cin, s2);
+	
+	if (s1.length() < s2.length()) {
+		swap(s1, s2);
+	}
+
+	if (s1.find(s2) != -1) {
+		cout << s2 << " is substring of " << s1;
+	}
+	else cout << "No substring";
+	return 0;
+}
+*/
